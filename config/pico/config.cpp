30f8bb977d3d2047a69d8f29af92cd4e6bab75d4
Abel Goy <agoy@ucsc.edu>
2024-02-14 15:02:32 -0800

    experimenting with new layout

--- config/pico/config.cpp
+++ config/pico/config.cpp
@@ -30,8 +30,8 @@ GpioButtonMapping button_mappings[] = {
     { &InputState::right,       2 },
     { &InputState::up2,         17 },
 
-    { &InputState::mod_x,       6 },
-    { &InputState::mod_y,       5 },
+    { &InputState::mod_x,       5 },
+    { &InputState::mod_y,       6 },
 
     { &InputState::select,      10},
     { &InputState::start,       0 },
@@ -39,19 +39,19 @@ GpioButtonMapping button_mappings[] = {
 
     { &InputState::c_left,      13},
     { &InputState::c_up,        12},
-    { &InputState::c_down,      15},
-    { &InputState::a,           14},
+    { &InputState::c_down,      14},
+    { &InputState::a,           26},
     { &InputState::c_right,     16},
 
-    { &InputState::b,           26},
-    { &InputState::x,           21},
+    { &InputState::b,           21},
+    { &InputState::x,           19},
     { &InputState::z,           27},
     { &InputState::up,          1},
 
     { &InputState::r,           20},
     { &InputState::y,           22},
     { &InputState::lightshield, 18},
-    { &InputState::midshield,   19},
+    { &InputState::midshield,   15},
 };
 size_t button_count = sizeof(button_mappings) / sizeof(GpioButtonMapping);
 
